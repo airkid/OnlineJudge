55c8919912465b14faf30e82f98c3536ffd336e7
LiQuidFly <LiQuidFly@126.com>
2015-04-09 23:41:50 +0800

    Judge.

--- OJ/judge.py
+++ OJ/judge.py
@@ -1,5 +1,33 @@
-__author__ = 'lqf'
+import subprocess
+from tempfile import TemporaryFile
 
-class complier:
-    def complie(self):
-        return
+from OJ.models import Submit,Answer
+
+ei = TemporaryFile(mode='w+')
+ei.write('2 3\n')
+ei.seek(0)
+
+o = TemporaryFile('w+')
+
+try:
+    p = subprocess.Popen("./run.sh 1 100000 ./test",shell=True,stdin=ei,stdout=o,universal_newlines=True)
+except:
+    print("except happen")
+p.wait()
+
+print("end")
+
+o.seek(0)
+print(o.readline())
+
+ei.close()
+o.close()
+
+class Judger:
+    path="../submit"
+    def __init__(self,id):
+        self._id=int(id)
+    def judge(self):
+        pass
+    def _complie(self):
+        pass

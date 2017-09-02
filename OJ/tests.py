415da27d42ac992201152b87a50f1024ff2f543b
Bluefissure <bluefissure@foxmail.com>
2017-09-02 12:08:56 +0800

    test

--- OJ/tests.py
+++ OJ/tests.py
@@ -5,17 +5,23 @@ import OJ.judger as judger
 from django.core.files.base import ContentFile
 class SubmitTest(TestCase):
     def setUp(self):
-        p1=Problem()
-        p1.input='1 2'
-        p1.output='3'
+        print("setUp start")
+        p1=Problem(uid=User())
+        t1=TestCase(pid=p1,uid=User())
+        t1.input='1 2'
+        t1.output='3'
         p1.save()
+        t1.save()
+        print("setUp finish")
         
 
     def test_submit(self):
+        print("test start")
     	sub = Submit(pid=Problem.objects.all()[0], uid=User(), lang=2)
         sub.save()
         content_file = ContentFile("#include <iostream>\nusing namespace std;\nint main()\n{\n    int a,b;\n    cin >> a >> b;\n    cout << a+b << endl;\n    return 0;\n}")
         sub.source_code.save(name=str(sub.id), content=content_file)
         sub.save()
         judger.Judger(sub);
+        print("test finish")
 

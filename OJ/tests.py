df7244d1b1f31d7e457e2fea063a2b6d67c82f34
Bluefissure <bluefissure@foxmail.com>
2017-09-02 12:24:40 +0800

    test

--- OJ/tests.py
+++ OJ/tests.py
@@ -6,22 +6,26 @@ from django.core.files.base import ContentFile
 class SubmitTest(TestCase):
     def setUp(self):
         print("setUp start")
-        p1=Problem(uid=User())
-        t1=TestCase(pid=p1,uid=User())
+        u=User()
+        p1=Problem(uid=u)
+        t1=TestCase(pid=p1,uid=u)
         t1.input='1 2'
         t1.output='3'
         p1.save()
         t1.save()
+        u.save()
         print("setUp finish")
         
 
     def test_submit(self):
         print("test start")
-    	sub = Submit(pid=Problem.objects.all()[0], uid=User(), lang=2)
+        u=User()
+    	sub = Submit(pid=Problem.objects.all()[0], uid=u, lang=2)
         sub.save()
         content_file = ContentFile("#include <iostream>\nusing namespace std;\nint main()\n{\n    int a,b;\n    cin >> a >> b;\n    cout << a+b << endl;\n    return 0;\n}")
         sub.source_code.save(name=str(sub.id), content=content_file)
         sub.save()
+        u.save()
         judger.Judger(sub);
         print("test finish")
 
